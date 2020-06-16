d0664f34b3e2b33ce029801412c18bc63996a962
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-16 20:57:43 +0100

    string_as_number passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -23,13 +23,9 @@ def length_of_string(test_string)
 end
 
 def join_string(string_1, string_2)
-    string_1 = "Mary had a little lamb, "
-    string_2 = "its fleece was white as snow"
-    return string_1 + string_2
+    return `#{string_1}, #{string_2}`
 end 
-    print(join_string)
 
-    def string_as_number(string_1 , string_2)
-    return string_one.to_i + string_2.to_i
+def string_as_number(string_1, string_2)
+    return string_1.to_i + string_2.to_i
 end
-    p string_as_number("1", "2")
