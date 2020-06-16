f2247aca6cdfa37bdd7502ea1c1428c3d3ee876f
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-16 20:55:36 +0100

    length_of_string passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,31 +1,26 @@
 def return_10
-return 10
-
+    return 10
 end
 
 def add(first_num, second_num)
     return first_num + second_num
 end
-print add(1,2)
 
 def subtract(first_num, second_num)
     return first_num - second_num
 end
-print subtract(10,5)
 
 def multiply(first_num, second_num)
     return first_num * second_num
 end
-print multiply(4,2)
 
 def divide(first_num, second_num)
     return first_num / second_num
 end
-print (10,2)
 
-# def length_of_string(test_string)
-#     test_string = "A string of length 21"
-#     return test_string
+def length_of_string(test_string)
+    return test_string.length
+end
 
 def join_string(string_1, string_2)
     string_1 = "Mary had a little lamb, "
